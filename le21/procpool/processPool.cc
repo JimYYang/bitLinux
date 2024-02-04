b0441959f9cafdbfea9f57421b54a1eb427dd43d
JimYYang <1729135661@qq.com>
2024-02-04 17:08:39 +0800

    完成通信管道进程池设计

--- le21/procpool/processPool.cc
+++ le21/procpool/processPool.cc
@@ -12,28 +12,25 @@
 #include <ctime>
 
 #define PROCESS_NUM 5
-
 #define MAKESEED() srand((unsigned long)time(nullptr) ^ getpid() ^ 0x17237 ^ rand() % 1234)
-
-typedef function<void()> func_t;
+typedef std::function<void()> func_t;
 using namespace std;
-
 
 void downloadTask()
 {
-    cout << "下载任务" << endl;
+    cout << getpid() << ": 下载任务\n" << endl;
     sleep(1);
 }
 
 void ioTask()
 {
-    cout << "io任务" << endl;
+    cout << getpid() << ": io任务\n" << endl;
     sleep(1);
 }
 
 void flushTask()
 {
-    cout << "flush任务" << endl;
+    cout << getpid() <<": flush任务\n" << endl;
     sleep(1);
 }
 
@@ -44,7 +41,6 @@ void loadTaskFunc(vector<func_t> &out)
     out.push_back(ioTask);
     out.push_back(flushTask);
 }
-
 
 class subEp
 {
@@ -63,10 +59,21 @@ public:
     int writeFd_;
 
 };
+
 int subEp::num = 0;
+
+int recvTask(int readFd)
+{
+    int code = 0;
+    ssize_t s = read(readFd, &code, sizeof(code));
+    if (s == 4) return code;
+    else if (s <= 0) return -1;
+    else return 0;
+}
 
 void createSubProcess(vector<subEp> &subs, vector<func_t> &funcMap)
 {
+    vector<int> deleteFd;
     for (int i = 0; i < PROCESS_NUM; i++)
     {
         int fds[2];
@@ -76,34 +83,51 @@ void createSubProcess(vector<subEp> &subs, vector<func_t> &funcMap)
         pid_t id = fork();
         if (id == 0)
         {
-            close(fds[0]);
+            // 关闭前面的写端
+            for (int i = 0; i < deleteFd.size(); i++)
+            {
+                close(deleteFd[i]);
+            }
+            // 子进程 进行处理任务
+            close(fds[1]);
+            while (true)
+            {
+                // 1. 获取命令码 如果没有发送 紫进程应该阻塞
+                int commandCode = recvTask(fds[0]);
+                // 2. 完成任务
+                if (commandCode >= 0 && commandCode < funcMap.size())
+                {
+                    funcMap[commandCode]();
+                }
+                else if (commandCode == -1)
+                {
+                    break;
+                }
+            }
             exit(0);
         }
-        close(fds[1]);
+        close(fds[0]);
         subs.push_back(subEp(id, fds[1]));
+        deleteFd.push_back(fds[1]);
     }
 
 }
 
-void sendTask(const subEp &process, int taskId)
+void sendTask(const subEp &process, int taskNum)
 {
-    
+    cout << "send task num: " << taskNum << " send to -> " << process.name_ << endl;
+    int n = write(process.writeFd_, &taskNum, sizeof(taskNum));
+    assert(n == sizeof(int));
+    (void)n;
 }
 
-
-int main()
+void loadBlanceControl(const vector<subEp> &subs, const vector<func_t> &funcMap, int cnt)
 {
-    MAKESEED();
-    vector<func_t> funcMap;
-    loadTaskFunc(funcMap);
-
-    vector<subEp> subs;
-    createSubProcess(subs, funcMap);
     int processNum = subs.size();
     int taskNum = funcMap.size();
 
-    bool quit = false;
-    while (!quit)
+    bool quit = cnt == 0;
+    while (true)
     {
         // 1. select son process
         int subIdx = rand() % processNum;
@@ -112,6 +136,47 @@ int main()
         int taskIdx = rand() % taskNum;
         // 3. send task to process
         sendTask(subs[subIdx], taskIdx);
+
+        sleep(1);
+
+        if (!quit)
+        {
+            if (--cnt == 0)
+                break;
+        }
     }
 
+    for (int i = 0; i < processNum; i++) close(subs[i].writeFd_);
+}
+
+void waitProcess(vector<subEp> &processes)
+{
+    int processNum = processes.size();
+    for (int i = 0; i < processNum; i ++)
+    {
+        waitpid(processes[i].subId_, nullptr, 0);
+        cout << "wait sub process success...: " << processes[i].subId_ << endl;
+    }
+}
+
+int main()
+{
+    MAKESEED();
+    // 1. 建立子进程和子进程通信的管道
+    // 1.1 加载方法表
+    vector<func_t> funcMap;
+    loadTaskFunc(funcMap);
+
+    // 1.2 创建子进程 并且维护好父子通信的信道
+    vector<subEp> subs;
+    createSubProcess(subs, funcMap);
+
+    // 2. 这里是父进程 控制子进程 负载均衡地向子进程发送命令码
+    int taskCnt = 3;
+    loadBlanceControl(subs, funcMap, taskCnt);
+
+
+    // 3. 回收子进程信息
+    waitProcess(subs);
+    return 0;
 }
